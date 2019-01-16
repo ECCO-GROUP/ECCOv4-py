372b44673c74185a7e2e6119bb43923021dc32d1
Ian Fenty <ifenty@gmail.com>
2019-01-15 17:55:00 -0800

    fixed tile plotting and resample to latlon (use linspace for integer steps in numpy)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
   name = 'ecco_v4_py',
   packages = ['ecco_v4_py'], # this must be the same as the name above
-  version = '0.18',
+  version = '0.181',
   description = 'Estimating the Circulation and Climate of the Ocean (ECCO) Version 4 Python Package',
   author = 'Ian Fenty',
   author_email = 'ifenty@gmail.com',
