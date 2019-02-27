f0e170da7c6140c2a5263ed4727a5334f0d598a2
Ou Wang <owang@penguin.jpl.nasa.gov>
2019-02-26 21:42:33 -0800

    Modify the subroutine months2days to add the month of time of origin as an extra input.

--- ecco_v4_py/dataset_utils.py
+++ ecco_v4_py/dataset_utils.py
@@ -39,13 +39,14 @@ def minimal_metadata(ds):
         if key in myDict: 
             del ds.attrs[key]
          
-def months2days(nmon=288, baseyear=1992):
+def months2days(nmon=288, baseyear=1992, basemon=1):
     
-    """
+    """ 
     This rouine converts time in days from January 1st of a particular year.
     
     Input:  nmon:           number of months (dtype=integer) 
             baseyear:       year of time of origin (dtype=integer)
+            basemon:        month of time of origin (dtype=integer)
     Output: time_days:      the middle time of each month in days
                                 from 01/01/baseyear (numpy array [nmon], dtype=double)
             time_days_bnds: time bounds (numpy array [nmon, 2], dtype=double)
@@ -56,20 +57,23 @@ def months2days(nmon=288, baseyear=1992):
     time_days_bnds = np.zeros([nmon,2])
     time_1stdayofmon = np.zeros([nmon+1])
     
-    basetime = datetime.datetime(baseyear, 1, 1, 0, 0, 0)
+    basetime = datetime.datetime(baseyear, basemon, 1, 0, 0, 0)
 
     for mon in range(nmon+1):
-        yrtmp = mon//12+baseyear
-        montmp = mon%12+1
+        #monfrombasemon is how many months fron basemon
+        monfrombasemon=basemon+mon-1
+        yrtmp = monfrombasemon//12+baseyear
+        montmp = monfrombasemon%12+1
         tmpdate = datetime.datetime(yrtmp,montmp,1,0,0,0)-basetime
         time_1stdayofmon[mon] = tmpdate.days
-#time bounds are the 1st day of each month.
+    #time bounds are the 1st day of each month.
     time_days_bnds[:,0]= time_1stdayofmon[0:nmon]
     time_days_bnds[:,1]= time_1stdayofmon[1:nmon+1]
-#center time of each month is the mean of the time bounds.    
+    #center time of each month is the mean of the time bounds.    
     time_days = np.mean(time_days_bnds,axis=1)
 
     ansi_datetmp = np.array([basetime + datetime.timedelta(days=time_days[i]) for i in xrange(nmon)])
     ansi_date = [str.replace(ansi_datetmp[i].isoformat(),'T',' ') for i in range(nmon)]
 
     return time_days, time_days_bnds, ansi_date
+
