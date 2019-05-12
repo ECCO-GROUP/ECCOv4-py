5a06389b61b573d61ddbc711045b0c05b9eb9e9b
Ian Fenty <ifenty@gmail.com>
2019-05-11 23:19:26 -0700

    vn

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
   name = 'ecco_v4_py',
   packages = ['ecco_v4_py'], # this must be the same as the name above
-  version = '1.0.2',
+  version = '1.0.3',
   description = 'Estimating the Circulation and Climate of the Ocean (ECCO) Version 4 Python Package',
   author = 'Ian Fenty',
   author_email = 'ian.fenty@jpl.nasa.gov',
@@ -17,7 +17,6 @@ setup(
 	'pyshp',
 	'proj',
 	'six',
-	'cartopy',
 	'dask[complete]',
 	'datetime',
 	'python-dateutil',
@@ -28,7 +27,8 @@ setup(
 	'xmitgcm',
 	'pyyaml',
 	'pyproj',
-	'pykdtree'],
+	'pykdtree',
+	'cartopy'],
   classifiers=[
       'Development Status :: 3 - Alpha',
       'Intended Audience :: Science/Research', 
