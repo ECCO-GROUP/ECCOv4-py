2cbe68cd20e1e6f5be60fdcb9358da75bafc6509
Ian Fenty <ifenty@gmail.com>
2019-05-17 03:44:07 -0700

    tile io updates

--- ecco_v4_py/tile_io.py
+++ ecco_v4_py/tile_io.py
@@ -22,35 +22,35 @@ def load_ecco_grid_from_tiles_nc(grid_dir,
                                  grid_base_name = 'ECCOv4r3_grid_tile_',
                                  tiles_to_load=range(13), 
                                  k_subset = [], 
-                                 use_dask = True, 
+                                 dask_chunk = True, 
                                  coords_as_vars = False):
         
-    num_loaded_tiles = 0
     g = []
     
     for i in tiles_to_load:
-        if use_dask:
-            g_i = xr.open_dataset(grid_dir + '/' + grid_base_name + str(i).zfill(2) + '.nc', chunks=90)
-        else:
-            g_i = xr.open_dataset(grid_dir + '/' + grid_base_name + str(i).zfill(2) + '.nc')
-       
-        if len(k_subset) > 0:
-                g_i = g_i.isel(k=k_subset)
-                
-        num_loaded_tiles += 1
         
-        if num_loaded_tiles == 1:
-            g = g_i
+       
+        if dask_chunk:
+            g_i = xr.open_dataset(grid_dir + '/' + \
+                              grid_base_name + \
+                              str(i).zfill(2) + '.nc', chunks = 90)
         else:
-            g = xr.concat((g, g_i ),'tile')
+            g_i = xr.open_dataset(grid_dir + '/' + \
+                              grid_base_name + \
+                              str(i).zfill(2) + '.nc')
+
+        if len(k_subset) > 0:
+            g_i = g_i.isel(k=k_subset)
             
-#    for key, value in ds.variables.items():
-#        if key not in ds.coords and len(ds[key].shape) >= 2:
-#            ds.variables[key].attrs['grid_layout'] = 'original llc'
+      
+        if isinstance(g, list):
+            g = g_i
             
-        if coords_as_vars:
-            g.reset_coords()
-            
+        else:     
+            g = xr.concat((g, g_i),'tile')
+    
+    if coords_as_vars:
+        g.reset_coords()
             
     return g  
 
@@ -60,18 +60,23 @@ def load_ecco_grid_from_tiles_nc(grid_dir,
 def recursive_load_ecco_var_from_tiles_nc(data_root_dir, 
                                           vars_to_load = 'all',
                                           tiles_to_load = range(13),
+                                          years_to_load = 'all',
                                           k_subset = [],
-                                          use_dask = True,
+                                          dask_chunk = True,
                                           less_output = True):
     
     g = []
 
-
-    # if 'ALL' is passed as the var_names then search in data_door_dir
-    # for variables.  
+ 
     if not isinstance(vars_to_load, list):
         vars_to_load = [vars_to_load]
     
+    if not isinstance(years_to_load,list):
+        years_to_load = [years_to_load]
+    
+    if not isinstance(k_subset, list):
+        k_subset = [k_subset]
+        
     if 'all' in vars_to_load:
         var_head_dirs = np.sort(glob.glob(data_root_dir + '/*/'))
         var_names = []
@@ -79,12 +84,15 @@ def recursive_load_ecco_var_from_tiles_nc(data_root_dir,
         for var in var_head_dirs:
            var_names.append(str.split(var,'/')[-2])
        
-        print ('searching for variables in ', data_root_dir)
-        if len(var_names) == 0:
-           print ('no variable names as top level directories in  %s' % \
-                  data_root_dir)
-        else:
-           print ('variables found: %s ' % str(var_names))
+        if not less_output:
+            print ('searching for variables in ', data_root_dir)
+
+        if not less_output:
+            if len(var_names) == 0:
+                print ('no variable names as top level directories in  %s' % \
+                      data_root_dir)
+            else:
+               print ('variables found: %s ' % str(var_names))
            
         vars_to_load = var_names
         
@@ -107,13 +115,15 @@ def recursive_load_ecco_var_from_tiles_nc(data_root_dir,
         top_level_dirs_with_var[var_name] = []
     
     for top_level_dir in top_level_dirs:
+#        if not less_output:
         print ('searching %s for variables ' % top_level_dir)
         
         keep_looking = True
         next_dir = data_root_dir + '/' + top_level_dir
         
         while keep_looking:
-            print (next_dir)
+            if not less_output:
+                print (next_dir)
             x = next(os.walk(next_dir))
 
             if len(x[2]) == 0 and len(x[1]) != 0:
@@ -126,7 +136,8 @@ def recursive_load_ecco_var_from_tiles_nc(data_root_dir,
                 if var_name_here in vars_to_load:
                     top_level_dirs_with_var[var_name_here].append(data_root_dir + '/' + top_level_dir + '/')
     
-    print(top_level_dirs_with_var)
+    if not less_output:
+        print(top_level_dirs_with_var)
    
     #%%
         
@@ -138,12 +149,12 @@ def recursive_load_ecco_var_from_tiles_nc(data_root_dir,
         #if not less_output:
         dirs_with_var = top_level_dirs_with_var[var_name]
         
-        print ('loading %s ' % var_name)
         if len(dirs_with_var) > 0:
-            print ('have directories with %s ' % var_name)
+            print ('located directories with %s ' % var_name)
             
             for dir_with_var in dirs_with_var:
-                print ('searching %s ' % dir_with_var)
+                if not less_output:
+                    print ('searching %s ' % dir_with_var)
                 sub_dirs = np.sort(([[x[0] for x in os.walk(dir_with_var)]]))[0]
 
                 for sub_dir in sub_dirs:
@@ -152,21 +163,46 @@ def recursive_load_ecco_var_from_tiles_nc(data_root_dir,
                         print (sub_dir)
                     
                     files = np.sort(glob.glob(sub_dir + '/' + var_name + '*nc'))
-                   
+                    
                     if len(files) > 0:
-                        if not less_output:
-                            print ('found files here .. loading %s ' % str(files))
+
+                        # by default, we'll load this directory of 
+                        # netcdf tile files. 
+                        load_this_directory = True
                         
-                        g_i = load_ecco_var_from_tiles_nc(sub_dir, 
-                                                           var_name, 
-                                                           tiles_to_load,
-                                                           k_subset, 
-                                                           use_dask, 
-                                                           less_output)
-                        if len(g_var) == 0:
-                            g_var = g_i
+                        # however, if the user specified a set of 
+                        # years to load then first check to make sure that
+                        # the year of these tile files is in the list...
+                        if 'all' not in years_to_load:
+                            # extract the first file
+                            file = files[0]
+                            # extract the year from the file name
+                            # (regular expression match 4 consecutive 
+                            # numbers) YYYY
+                            file_year =  int(re.findall(r"[0-9]{4}", file)[0])
+                                    
+                            if file_year not in years_to_load:
+                                load_this_directory = False
+
+                        if load_this_directory:
+                            if not less_output:
+                                print ('found files here .. loading %s ' % str(files))
+                            
+                            g_i = load_ecco_var_from_tiles_nc(sub_dir, 
+                                                               var_name, 
+                                                               tiles_to_load=tiles_to_load,
+                                                               k_subset=k_subset, 
+                                                               dask_chunk=dask_chunk, 
+                                                               less_output=less_output)
+                            if isinstance(g_var, list):
+                                g_var = g_i
+                            else:
+                                g_var = xr.concat((g_var, g_i),'time')
                         else:
-                            g_var = xr.concat((g_var, g_i),'time')
+                            if not less_output:
+                                print ('skipping files from year %s ' \
+                                       % file_year)
+                            
         else:
             print ('no subdirectories with %s ' % var_name)          
         
@@ -193,7 +229,7 @@ def load_ecco_var_from_tiles_nc(data_dir,
                                 var_name, 
                                 tiles_to_load = range(13), 
                                 k_subset = [], 
-                                use_dask = True,
+                                dask_chunk = True,
                                 less_output = True):
     
     g = []    
@@ -212,16 +248,20 @@ def load_ecco_var_from_tiles_nc(data_dir,
             if tile in tiles_to_load:
                 if not less_output:
                     print ('loading tile %d' % tile)
-                    
-                if use_dask:
-                    g_i = xr.open_dataset(file, chunks=90)
+    
+                if dask_chunk:
+                    g_i = xr.open_dataset(file, chunks = 90)
+
+                    #print ('chunking')
                 else:
                     g_i = xr.open_dataset(file)
-               
+                    #nt = len(g_i.time)
+                    #g_i = g_i.chunk(nx, nx)
+    
                 if len(k_subset) > 0 and \
                     'k' in g_i.coords.keys():
                     g_i = g_i.isel(k=k_subset)
-                    
+                                    
     
                 if len(g) == 0:
                     g = g_i
@@ -247,7 +287,7 @@ def load_ecco_var_from_tiles_nc(data_dir,
 #%%
 def load_ecco_var_from_years_nc(data_dir, var_name, years_to_load = 'all',
                                 tiles_to_load = range(13), k_subset = [],
-                                use_dask = True, less_output = True):
+                                dask_chunk = True, less_output = True):
     
     
     files = np.sort(glob.glob(data_dir + '/' + var_name + '*nc'))
@@ -257,7 +297,10 @@ def load_ecco_var_from_years_nc(data_dir, var_name, years_to_load = 'all',
     if not less_output:
         print ('--- LOADING %s FROM YEARS NC: %s' % \
                (var_name, data_dir))
-            
+     
+    if not(isinstance(years_to_load, list)):
+        years_to_load = [years_to_load]
+        
     if len(files) > 0:
         if not less_output:
             print ('---found %s nc files here.  loading ....' % var_name)
@@ -269,23 +312,24 @@ def load_ecco_var_from_years_nc(data_dir, var_name, years_to_load = 'all',
             if var_name_of_file == var_name:
                 
                 if 'all' in years_to_load or file_year in years_to_load:
-                        
-                    if use_dask:
-                        g_i = xr.open_dataset(file,chunks=90)
+                     
+                    if dask_chunk:
+                        g_i = xr.open_dataset(file, chunks=90)
                     else:
                         g_i = xr.open_dataset(file)
-                    
+
                     # pull out a k subset
                     if len(k_subset) > 0 and 'k' in g_i.coords.keys():
                         g_i = g_i.isel(k=k_subset)
     
                     # pull out the tile subset
                     g_i = g_i.isel(tile=tiles_to_load)
+
                     
-                    if len(g) == 0:
+                    if isinstance(g, list):
                         g = g_i
                     else:
-                        g = xr.concat([g, g_i],'time')
+                        g = xr.concat((g, g_i),'time')
         
 
         # finished looping through files
@@ -312,7 +356,7 @@ def recursive_load_ecco_var_from_years_nc(data_root_dir,
                                           years_to_load = 'all',
                                           tiles_to_load = range(13),
                                           k_subset = [],
-                                          use_dask = True,
+                                          dask_chunk = True,
                                           less_output = True):
  
     # ecco_dataset to return
@@ -363,11 +407,11 @@ def recursive_load_ecco_var_from_years_nc(data_root_dir,
                                              years_to_load = years_to_load,
                                              tiles_to_load = tiles_to_load,
                                              k_subset =  k_subset,
-                                             use_dask = use_dask,
+                                             dask_chunk = dask_chunk,
                                              less_output =less_output)
 
                 if len(g_i) > 0:
-                    if len(g_var) == 0:
+                    if isinstance(g_var, list):
                         g_var = g_i
                     else:
                         g_var = xr.concat((g_var,g_i),'time')
