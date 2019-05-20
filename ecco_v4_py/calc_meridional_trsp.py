b919101ce32a3825116d0fd01490ebd13e8ed14d
Timothy Smith <timsmith204@utexas.edu>
2019-05-20 09:32:20 -0700

    Trsp as dataset (#27)
    * change vol, heat, salt trsp to return dataset with depth wise trsp
    
    * catching some typos
    
    * typo
    
    * provide section vol, heat,salt trsp as datasets with depth wise trsp
    
    * meridional_trsp_at_depth now returns a dataset to retain the Z coordinate
    
    * return dataset for trsp at depth
    
    * formatting

--- ecco_v4_py/calc_meridional_trsp.py
+++ ecco_v4_py/calc_meridional_trsp.py
@@ -41,29 +41,39 @@ def calc_meridional_vol_trsp(ds,lat_vals,basin_name=None,grid=None):
 
     Returns
     -------
-    vol_trsp : xarray DataArray
-        meridional freshwater transport in Sv
-        with dimensions 'time' (if in given dataset) and 'lat' 
+    ds_out : xarray Dataset
+        Dataset with the following variables
+            'vol_trsp' :
+                meridional volume transport in Sv
+                with dimensions 'time' (if in given dataset) and 'lat'
+            'vol_trsp_z' :
+                meridional volume transport in Sv at each depth level
+                in the given input fields
+                dimensions: 'time' (if provided), 'lat', and 'k'
     """
 
     x_vol = ds['UVELMASS'] * ds['drF'] * ds['dyG']
     y_vol = ds['VVELMASS'] * ds['drF'] * ds['dxG']
 
     # Computes salt transport in m^3/s at each depth level
-    vol_trsp = meridional_trsp_at_depth(x_vol,y_vol,
-                                       cds=ds.coords.to_dataset(),
-                                       lat_vals=lat_vals,
-                                       basin_name=basin_name,
-                                       grid=grid)
+    ds_out = meridional_trsp_at_depth(x_vol,y_vol,
+                                      cds=ds.coords.to_dataset(),
+                                      lat_vals=lat_vals,
+                                      basin_name=basin_name,
+                                      grid=grid)
 
-    # Sum over depth
-    vol_trsp = vol_trsp.sum('k')
+    # Rename to useful data array name
+    ds_out = ds_out.rename({'trsp_z': 'vol_trsp_z'})
 
-    # Convert to Sv
-    vol_trsp = METERS_CUBED_TO_SVERDRUPS * vol_trsp
-    vol_trsp.attrs['units'] = 'Sv'
+    # Sum over depth for total transport
+    ds_out['vol_trsp'] = ds_out['vol_trsp_z'].sum('k')
 
-    return vol_trsp
+    # Convert both fields to Sv
+    for fld in ['vol_trsp','vol_trsp_z']:
+        ds_out[fld] = METERS_CUBED_TO_SVERDRUPS * ds_out[fld]
+        ds_out[fld].attrs['units'] = 'Sv'
+
+    return ds_out
 
 
 def calc_meridional_heat_trsp(ds,lat_vals,basin_name=None,grid=None):
@@ -78,30 +88,39 @@ def calc_meridional_heat_trsp(ds,lat_vals,basin_name=None,grid=None):
 
     Returns
     -------
-    heat_trsp : xarray DataArray
-        meridional heat transport in petawatts
-        with dimensions 'time' (if in given dataset) and 'lat' 
+    ds_out : xarray Dataset
+        Dataset with the following variables
+            'heat_trsp' :
+                meridional heat transport in PW
+                with dimensions 'time' (if in given dataset) and 'lat'
+            'heat_trsp_z' :
+                meridional heat transport in PW at each depth level
+                in the given input fields
+                dimensions: 'time' (if provided), 'lat', and 'k'
     """
 
     x_heat = ds['ADVx_TH'] + ds['DFxE_TH']
     y_heat = ds['ADVy_TH'] + ds['DFyE_TH']
 
     # Computes heat transport in degC * m^3/s at each depth level
-    heat_trsp = meridional_trsp_at_depth(x_heat,y_heat,
-                                         cds=ds.coords.to_dataset(),
-                                         lat_vals=lat_vals,
-                                         basin_name=basin_name,
-                                         grid=grid)
+    ds_out = meridional_trsp_at_depth(x_heat,y_heat,
+                                      cds=ds.coords.to_dataset(),
+                                      lat_vals=lat_vals,
+                                      basin_name=basin_name,
+                                      grid=grid)
 
-    # Sum over depth
-    heat_trsp = heat_trsp.sum('k')
+    # Rename to useful data array name
+    ds_out = ds_out.rename({'trsp_z': 'heat_trsp_z'})
 
-    # Convert to PetaWatts
-    heat_trsp = WATTS_TO_PETAWATTS * RHO_CONST * HEAT_CAPACITY * heat_trsp
+    # Sum over depth for total transport
+    ds_out['heat_trsp'] = ds_out['heat_trsp_z'].sum('k')
 
-    heat_trsp.attrs['units'] = 'PW'
+    # Convert both fields to PW
+    for fld in ['heat_trsp','heat_trsp_z']:
+        ds_out[fld] = WATTS_TO_PETAWATTS * RHO_CONST * HEAT_CAPACITY * ds_out[fld]
+        ds_out[fld].attrs['units'] = 'PW'
 
-    return heat_trsp
+    return ds_out
 
 def calc_meridional_salt_trsp(ds,lat_vals,basin_name=None,grid=None):
     """Compute salt transport across latitude band in psu * Sv
@@ -115,29 +134,39 @@ def calc_meridional_salt_trsp(ds,lat_vals,basin_name=None,grid=None):
 
     Returns
     -------
-    salt_trsp : xarray DataArray
-        meridional salt transport in psu*Sv
-        with dimensions 'time' (if in given dataset) and 'lat' 
+    ds_out : xarray Dataset
+        Dataset with the following variables
+            'salt_trsp' :
+                meridional salt transport in psu*Sv
+                with dimensions 'time' (if in given dataset) and 'lat'
+            'salt_trsp_z' :
+                meridional salt transport in psu*Sv at each depth level
+                in the given input fields
+                dimensions: 'time' (if provided), 'lat', and 'k'
     """
 
     x_salt = ds['ADVx_SLT'] + ds['DFxE_SLT']
     y_salt = ds['ADVy_SLT'] + ds['DFyE_SLT']
 
     # Computes salt transport in psu * m^3/s at each depth level
-    salt_trsp = meridional_trsp_at_depth(x_salt,y_salt,
-                                         cds=ds.coords.to_dataset(),
-                                         lat_vals=lat_vals,
-                                         basin_name=basin_name,
-                                         grid=grid)
-    # Sum over depth
-    salt_trsp = salt_trsp.sum('k')
+    ds_out = meridional_trsp_at_depth(x_salt,y_salt,
+                                      cds=ds.coords.to_dataset(),
+                                      lat_vals=lat_vals,
+                                      basin_name=basin_name,
+                                      grid=grid)
 
-    # Convert to psu * Sv
-    salt_trsp = METERS_CUBED_TO_SVERDRUPS * salt_trsp
-    salt_trsp.attrs['units'] = 'psu.Sv'
+    # Rename to useful data array name
+    ds_out = ds_out.rename({'trsp_z': 'salt_trsp_z'})
 
-    return salt_trsp
+    # Sum over depth for total transport
+    ds_out['salt_trsp'] = ds_out['salt_trsp_z'].sum('k')
 
+    # Convert both fields to psu.Sv
+    for fld in ['salt_trsp','salt_trsp_z']:
+        ds_out[fld] = METERS_CUBED_TO_SVERDRUPS * ds_out[fld]
+        ds_out[fld].attrs['units'] = 'psu.Sv'
+
+    return ds_out
 
 # ---------------------------------------------------------------------
 
@@ -165,10 +194,12 @@ def meridional_trsp_at_depth(xfld, yfld, lat_vals, cds,
 
     Returns
     -------
-    lat_trsp : xarray DataArray
-        transport of vector quantity across denoted latitude band at
-        each depth level with dimensions 'time' (if in given dataset),
-        'k' (depth), and 'lat' 
+    ds_out : xarray Dataset
+        with the main variable
+            'trsp_z'
+                transport of vector quantity across denoted latitude band at
+                each depth level with dimensions 'time' (if in given dataset),
+                'k' (depth), and 'lat' 
     """
 
     if grid is None:
@@ -178,7 +209,7 @@ def meridional_trsp_at_depth(xfld, yfld, lat_vals, cds,
         lat_vals = [lat_vals]
 
     # Initialize empty DataArray with coordinates and dims
-    lat_trsp = _initialize_trsp_data_array(cds, lat_vals)
+    ds_out = _initialize_trsp_data_array(cds, lat_vals)
 
     # Get basin mask
     if basin_name is not None:
@@ -197,9 +228,9 @@ def meridional_trsp_at_depth(xfld, yfld, lat_vals, cds,
         lat_trsp_x = (xfld * lat_maskW * basin_maskW).sum(dim=['i_g','j','tile'])
         lat_trsp_y = (yfld * lat_maskS * basin_maskS).sum(dim=['i','j_g','tile'])
 
-        lat_trsp.loc[{'lat':lat}] = lat_trsp_x + lat_trsp_y
+        ds_out['trsp_z'].loc[{'lat':lat}] = lat_trsp_x + lat_trsp_y
 
-    return lat_trsp
+    return ds_out
 
 
 def _initialize_trsp_data_array(cds, lat_vals):
@@ -215,8 +246,13 @@ def _initialize_trsp_data_array(cds, lat_vals):
 
     Returns
     -------
-    da : xarray DataArray
-        zero-valued DataArray with time (optional), depth, and latitude dimensions
+    ds_out : xarray Dataset
+        Dataset with the variables
+            'trsp_z'
+                zero-valued DataArray with time (optional), 
+                depth, and latitude dimensions
+            'Z'
+                the original depth coordinate
     """
 
     coords = OrderedDict()
@@ -238,5 +274,12 @@ def _initialize_trsp_data_array(cds, lat_vals):
 
     dims += ('k','lat')
 
-    return xr.DataArray(data=zeros, coords=coords, dims=dims)
+    xda = xr.DataArray(data=zeros, coords=coords, dims=dims)
+
+    # Convert to dataset to add Z coordinate
+    xds = xda.to_dataset(name='trsp_z')
+    xds['Z'] = cds['Z']
+    xds = xds.set_coords('Z')
+
+    return xds
 
