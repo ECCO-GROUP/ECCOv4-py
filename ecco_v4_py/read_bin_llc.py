2fd134e03402e170b1153b67e1dcc2e6a1eb99e0
Ou Wang <owang@penguin.jpl.nasa.gov>
2019-08-16 11:36:36 -0700

    Fix the incorrect long_time for the tile attr.

--- ecco_v4_py/read_bin_llc.py
+++ ecco_v4_py/read_bin_llc.py
@@ -197,7 +197,7 @@ def load_ecco_vars_from_mds(mds_var_dir,
        
     if 'iter' in ecco_dataset.coords.keys():
         ecco_dataset = ecco_dataset.rename({'iter': 'timestep'})
-        #xgcm aleardy has set the long_name for timestep.
+        #xmitgcm aleardy has set the long_name for timestep.
         #ecco_dataset.timestep.attrs['long_name'] = 'model time step'
     # if vars_to_load is an empty list, keep all variables.  otherwise,
     # only keep those variables in the vars_to_load list.
