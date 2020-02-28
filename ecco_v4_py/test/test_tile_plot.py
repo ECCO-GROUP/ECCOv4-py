968238e9586326862f95eb865561c7726b8be635
Timothy Smith <timsmith204@utexas.edu>
2020-02-28 15:18:13 -0600

    tests for tile_plot and plot_utils

--- ecco_v4_py/test/test_tile_plot.py
+++ ecco_v4_py/test/test_tile_plot.py
@@ -8,6 +8,8 @@ import numpy as np
 import pytest
 import ecco_v4_py as ecco
 
+from .test_plot_utils import get_test_array
+
 # Define bin directory for test reading
 _PKG_DIR = Path.cwd().resolve().parent.parent
 _DATA_DIR = _PKG_DIR.joinpath('binary_data')
@@ -16,31 +18,58 @@ _TEST_FILES = ['basins.data', 'hFacC.data', 'state_3d_set1.0000000732.data']
 _TEST_NK = [1, 50, 50]
 _TEST_RECS = [1, 1, 3]
 
-def get_default_plot_tiles_input():
-    """make a dict with default inputs"""
-    return {'cmap':None,
-            'layout':'llc',
-            'rotate_to_latlon':False,
-            'show_colorbar':False,
-            'show_cbar_label':False,
-            'show_tile_labels':True,
-            'fig_size':9,
-            'less_output':True}
-
-@pytest.mark.parametrize("fname,vdict",[
-        (_TEST_FILES[0],{}), # test defaults
-        (_TEST_FILES[0],{'layout':'latlon', 
-                         'rotate_to_latlon':False),
-        (_TEST_FILES[0],{'layout':'latlon',
-                         'rotate_to_latlon':False,
-                         'Arctic_cap_tile_location':5}),
+@pytest.mark.parametrize("vdict",[
+        {}, #defaults
+        {'cmap':'plasma'},
+        {'layout':'latlon', 
+          'rotate_to_latlon':False},
+        {'layout':'latlon',
+         'rotate_to_latlon':False,
+         'Arctic_cap_tile_location':5},
+        {'layout':'latlon',
+         'rotate_to_latlon':False,
+         'Arctic_cap_tile_location':7},
+        {'layout':'latlon',
+         'rotate_to_latlon':False,
+         'Arctic_cap_tile_location':10},
+        {'show_colorbar':True},
+        {'show_colorbar':True,
+         'show_cbar_label':True,
+         'cbar_label':'something good'},
+        {'show_tile_labels':False},
+        {'fig_size':20},
+        {'less_output':False}
     ])
-def test_plot_tiles(fname,vdict):
+def test_plot_tiles(vdict):
     """Run through various options and make sure nothing is broken"""
 
-    #get defaults
-    inputs=get_default_plot_tiles_input()
-    for key, val in vdict.items():
-        inputs[key] = val
+    # read in array
+    nparr = get_test_array(is_xda=False)
+    xda = get_test_array(is_xda=True)
 
-    # 
+    # run plotting routine
+    for arr in [nparr,xda]:
+        ecco.plot_tiles(arr, **vdict)
+
+@pytest.mark.parametrize("vdict",[
+        {}, #defaults
+        {'cmap':'plasma'},
+        {'show_colorbar':True},
+        {'show_colorbar':True,
+         'show_cbar_label':True,
+         'cbar_label':'something good'},
+        {'show_tile_labels':False},
+        {'less_output':False}
+    ])
+def test_plot_single_tile(vdict):
+    """plot a single tile"""
+
+    # read in array
+    nparr = get_test_array(is_xda=False)
+    xda = get_test_array(is_xda=True)
+
+    # run plotting routine on each tile
+    for t in xda.tile.values:
+
+        ecco.plot_tile(nparr[t,...], **vdict)
+        ecco.plot_tile(xda.sel(tile=t), **vdict)
