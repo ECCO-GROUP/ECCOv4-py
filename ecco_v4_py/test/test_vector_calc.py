3d39041b2e3b9a7f7c8c61b1d5ef9e1dc80bfc56
Timothy Smith <timsmith204@utexas.edu>
2020-10-20 14:55:12 -0600

    finally a test for latitude masks...

--- ecco_v4_py/test/test_vector_calc.py
+++ ecco_v4_py/test/test_vector_calc.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import numpy as np
 import xarray as xr
 import pytest
-from ecco_v4_py import vector_calc
+from ecco_v4_py import vector_calc, get_llc_grid
 
 from .test_common import llc_mds_datadirs, get_test_vectors
 
@@ -15,7 +15,7 @@ def test_no_angles(get_test_vectors):
     """quick error handling test"""
 
     ds = get_test_vectors
-    ds = ds.drop(['CS','SN'])
+    ds = ds.drop_vars(['CS','SN'])
     with pytest.raises(KeyError):
         vector_calc.UEVNfromUXVY(ds['U'],ds['V'],ds)
 
@@ -53,3 +53,32 @@ def test_uevn_from_uxvy(get_test_vectors):
     for t in [1,4,8,11]:
         assert np.allclose(uE.sel(tile=t),1,atol=1e-15)
         assert np.allclose(vN.sel(tile=t),1,atol=1e-15)
+
+def test_latitude_masks(get_test_vectors):
+    """run through lats, and ensure we're grabbing the closest
+    "south-grid-cell-location" (whether that's in the x or y direction!)
+    to each latitude value"""
+
+    ds = get_test_vectors
+
+    grid = get_llc_grid(ds)
+    yW = grid.interp(ds['YC'],'X',boundary='fill')
+    yS = grid.interp(ds['YC'],'Y',boundary='fill')
+    wetW = ds['maskW'].isel(k=0)
+    wetS = ds['maskS'].isel(k=0)
+
+    dLat = 0.5 # is this robust?
+    nx = 90
+
+    for lat in np.arange(-89,89):
+        print('lat: ',lat)
+        maskW,maskS = vector_calc.get_latitude_masks(lat,ds['YC'],grid)
+
+        maskW = maskW.where((maskW!=0) & wetW,0.)
+        maskS = maskS.where((maskS!=0) & wetS,0.)
+
+        assert not (maskW>0).sel(tile=slice(7,None)).any()
+        assert not (maskS<0).sel(tile=slice(5)).any()
+
+        assert (yW-lat < dLat).where(ds['maskW'].isel(k=0) & (maskW!=0)).all().values
+        assert (yS-lat < dLat).where(ds['maskS'].isel(k=0) & (maskS!=0)).all().values
