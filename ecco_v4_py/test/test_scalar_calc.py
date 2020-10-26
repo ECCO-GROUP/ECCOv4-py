99149a026b3aa6260d3f9bc785dd3cffb140d8e8
Timothy Smith <timsmith204@utexas.edu>
2020-10-26 15:01:51 -0600

    test scalar calc module

---
+++ ecco_v4_py/test/test_scalar_calc.py
@@ -0,0 +1,33 @@
+"""
+Test routines for the vector calculations module
+"""
+from __future__ import division, print_function
+import warnings
+from pathlib import Path
+import numpy as np
+import xarray as xr
+import pytest
+from ecco_v4_py import scalar_calc, get_llc_grid
+
+from .test_common import llc_mds_datadirs, get_test_ds
+
+def test_latitude_mask(get_test_ds):
+    """run through lats, and ensure we're grabbing the closest
+    "south-grid-cell-location" (whether that's in the x or y direction!)
+    to each latitude value"""
+
+    ds = get_test_ds
+
+    grid = get_llc_grid(ds)
+    wetC = ds['maskC'].isel(k=0)
+
+    dLat = 0.5 # is this robust?
+    nx = 90
+
+    for lat in np.arange(-89,89):
+        print('lat: ',lat)
+        maskC = scalar_calc.get_latitude_mask(lat,ds['YC'],grid)
+
+        maskC = maskC.where((maskC!=0) & wetC,0.)
+
+        assert (ds['YC']-lat < dLat).where((maskC!=0)).all().values
