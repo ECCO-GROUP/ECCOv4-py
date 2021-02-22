f3f7b7e7d56f255adce76c6d416d85cc5ea3745b
Ian Fenty <ifenty@users.noreply.github.com>
2021-02-22 15:37:19 -0800

    updated tile_plot_proj to handle labels on polar stereographic and other plot projections now that they are supported by cartopy (#114)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def README():
 setup(
   name = 'ecco_v4_py',
   packages = ['ecco_v4_py'], # this must be the same as the name above
-  version = '1.4.3',
+  version = '1.4.4',
   description = 'Estimating the Circulation and Climate of the Ocean (ECCO) Version 4 Python Package',
   author = 'Ian Fenty',
   author_email = 'ian.fenty@jpl.nasa.gov',
