c8f3d43fc33c679709f16b065818c1de1f3e3397
Ian Fenty <ifenty@users.noreply.github.com>
2021-06-10 02:58:42 -0700

    updated version number (#125)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def README():
 setup(
   name = 'ecco_v4_py',
   packages = ['ecco_v4_py'], # this must be the same as the name above
-  version = '1.5.0',
+  version = '1.5.1',
   description = 'Estimating the Circulation and Climate of the Ocean (ECCO) Version 4 Python Package',
   author = 'Ian Fenty',
   author_email = 'ian.fenty@jpl.nasa.gov',
@@ -23,7 +23,7 @@ setup(
 	'pyresample',
 	'xarray',
 	'xmitgcm',
-	'cartopy',
+	'cartopy>=0.19.0',
     'netCDF4',
 	'xgcm>=0.5.0',
         'future',
