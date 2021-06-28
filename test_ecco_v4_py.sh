1f180f8126abe5c3de0608c851601f64ba000876
Ian Fenty <ifenty@users.noreply.github.com>
2021-06-28 14:14:58 -0700

    updated dependencies for CI and updated v to 1.5.2 (#126)

--- test_ecco_v4_py.sh
+++ test_ecco_v4_py.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# note pytest package must be installed
+# note pytest and pytest-cov packages must be installed
 
 # test on 1 cpu
-#py.test . -v --cov=ecco_v4_py --cov-config .coveragerc --ignore=ecco_v4_py/test/test_generate_ecco_netcdf_product.py
+py.test . -v --cov=ecco_v4_py --cov-config .coveragerc --ignore=ecco_v4_py/test/test_generate_ecco_netcdf_product.py
 
 # test with 12 cpus
 # must have pytest-xdist installed
-py.test . -v -n 12 --cov=ecco_v4_py --cov-config .coveragerc --ignore=ecco_v4_py/test/test_generate_ecco_netcdf_product.py
+#py.test . -v -n 12 --cov=ecco_v4_py --cov-config .coveragerc --ignore=ecco_v4_py/test/test_generate_ecco_netcdf_product.py
 
 rm -fr /tmp/pytest*
