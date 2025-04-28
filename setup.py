7aefc0513c219b726ec9e8edbd1829007a9698ad
Andrew Delman <andrew.s.delman@gmail.com>
2025-04-28 23:16:06 +0000

    retrying pypi v1.7.4 release as v1.7.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def README():
 setup(
   name = 'ecco_v4_py',
   packages = ['ecco_v4_py'], # this must be the same as the name above
-  version = '1.7.4',
+  version = '1.7.4.1',
   description = 'Estimating the Circulation and Climate of the Ocean (ECCO) Version 4 Python Package',
   author = 'Ian Fenty, Ou Wang, Tim Smith, Andrew Delman, and others',
   author_email = 'ian.fenty@jpl.nasa.gov, ecco-group@mit.edu',
