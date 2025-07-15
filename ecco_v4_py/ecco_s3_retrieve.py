5b003bfb14493f6ea8921a1d91ea8fc907770fa5
Andrew Delman <andrew.s.delman@gmail.com>
2025-07-15 23:03:02 +0000

    set asynchronous=False for v4r4 access in s3_open_fsspec mode

--- ecco_v4_py/ecco_s3_retrieve.py
+++ ecco_v4_py/ecco_s3_retrieve.py
@@ -604,7 +604,7 @@ def ecco_podaac_s3_open_fsspec(ShortName,version,jsons_root_dir=None,jsons_retri
                     remote_protocol="s3", 
                     remote_options={"anon":False,\
                                     "requester_pays":True},\
-                    asynchronous=True)
+                    asynchronous=False)
     else:
         # get NASA Earthdata credentials for S3
         creds = requests.get('https://archive.podaac.earthdata.nasa.gov/s3credentials').json()
